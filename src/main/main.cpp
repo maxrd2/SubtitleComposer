c6c69c0f448ff5172cc26618d8c6fb5de07fac73
Mladen Milinkovic <max@smoothware.net>
2016-03-15 21:48:55 +0100

    version 0.6.1

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -52,7 +52,7 @@ main(int argc, char **argv)
 	KAboutData aboutData(
 		"subtitlecomposer",
 		i18n("Subtitle Composer"),
-		"0.6.0",
+		"0.6.1",
 		i18n("A KDE subtitle editor."),
 		KAboutLicense::GPL,
 		"&copy; 2007-2012 Sergio Pistone\n&copy; 2013-2015 Mladen Milinković",
