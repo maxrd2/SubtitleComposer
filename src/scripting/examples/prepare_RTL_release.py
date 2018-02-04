8a4ded6293d1ef24642291b2f1168bb181ef4a14
Safa AlFulaij <safa1996alfulaij@gmail.com>
2018-02-04 15:10:17 +0300

    Fix formatting

--- src/scripting/examples/prepare_RTL_release.py
+++ src/scripting/examples/prepare_RTL_release.py
@@ -31,20 +31,20 @@ tempLine = s.line(s.linesCount()-1)
 
 # All the lines except the last one
 for line_index in range(0, s.linesCount()-1):
-    line = s.line( line_index )
+	line = s.line( line_index )
 
-    # To store the modefied sublines
-    combinedArray = []
+	# To store the modefied sublines
+	combinedArray = []
 
-    # each line in the subtitle line
-    for eachSubLine in line.richPrimaryText().split("\n"):
-        tempLine.setRichPrimaryText(eachSubLine)
-        if tempLine.isRightToLeft():
-            combinedArray.append(u"\u202B%s"%eachSubLine.decode('utf8')) # Add RLE for RTL lines
-        else:
-            combinedArray.append(u"\u202A%s"%eachSubLine.decode('utf8')) # Add LRE for LTR lines
+	# each line in the subtitle line
+	for eachSubLine in line.richPrimaryText().split("\n"):
+		tempLine.setRichPrimaryText(eachSubLine)
+		if tempLine.isRightToLeft():
+			combinedArray.append(u"\u202B%s"%eachSubLine.decode('utf8')) # Add RLE for RTL lines
+		else:
+			combinedArray.append(u"\u202A%s"%eachSubLine.decode('utf8')) # Add LRE for LTR lines
 
-    line.setRichPrimaryText("\n".join(combinedArray))
+	line.setRichPrimaryText("\n".join(combinedArray))
 
 # Delete the temp line
 s.removeLine(s.linesCount()-1)
