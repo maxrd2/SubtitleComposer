5ee16067d6e70167312e9c9e455a72ece1ed3ff9
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-08-23 07:19:19 +0200

    Clean up leftovers statements in license headers
    That information is already stated using the SPDX-License-Identifier statement

--- src/scripting/examples/example_trim_to_selection.js
+++ src/scripting/examples/example_trim_to_selection.js
@@ -2,16 +2,6 @@
     SPDX-FileCopyrightText: 2007-2009 Sergio Pistone (sergio_pistone@yahoo.com.ar)
 
     SPDX-License-Identifier: GPL-2.0-or-later
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the
-    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
 */
 
 subtitle.instance().removeLines( ranges.newSelectionRangeList().complement() );
