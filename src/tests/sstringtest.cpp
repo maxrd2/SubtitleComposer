b3b51685172d99b3a39d8a3666334802915b15bd
Mladen Milinkovic <maxrd2@smoothware.net>
2022-06-23 01:40:15 +0200

    SString: added/cleaned unit tests

--- src/tests/sstringtest.cpp
+++ src/tests/sstringtest.cpp
@@ -7,10 +7,12 @@
 
 #include "sstringtest.h"
 #include "core/sstring.h"
-
-#include <QTest>                               // krazy:exclude=c++/includes
+#include "helpers/common.h"
 
 #include <QDebug>
+#include <QTest>
+#include <QRegExp>
+#include <QRegularExpression>
 
 using namespace SubtitleComposer;
 
@@ -100,21 +102,21 @@ SStringTest::testInsert()
 
 	sstring.append(SString("x", SString::Bold));
 	QVERIFY(sstring.richString() == QLatin1String("<b>x</b>"));
-	sstring.append(QStringLiteral("y"));
+	sstring.append($("y"));
 	QVERIFY(sstring.richString() == QLatin1String("<b>xy</b>"));
 	sstring.append('z');
 	QVERIFY(sstring.richString() == QLatin1String("<b>xyz</b>"));
 
 	sstring.prepend(SString("c", SString::Italic));
 	QVERIFY(sstring.richString() == QLatin1String("<i>c</i><b>xyz</b>"));
-	sstring.prepend(QStringLiteral("b"));
+	sstring.prepend($("b"));
 	QVERIFY(sstring.richString() == QLatin1String("<i>bc</i><b>xyz</b>"));
 	sstring.prepend('a');
 	QVERIFY(sstring.richString() == QLatin1String("<i>abc</i><b>xyz</b>"));
 
 	sstring.insert(3, SString("g", SString::Underline));
 	QVERIFY(sstring.richString() == QLatin1String("<i>abc</i><u>g</u><b>xyz</b>"));
-	sstring.insert(4, QStringLiteral("h"));
+	sstring.insert(4, $("h"));
 	QVERIFY(sstring.richString() == QLatin1String("<i>abc</i><u>gh</u><b>xyz</b>"));
 	sstring.insert(5, 'i');
 	QVERIFY(sstring.richString() == QLatin1String("<i>abc</i><u>ghi</u><b>xyz</b>"));
@@ -128,22 +130,22 @@ SStringTest::testReplace()
 {
 	SString sstring;
 
-	// SString& replace( int index, int len, const QString& replacement )
+	// SString & SString::replace(int index, int len, const QString &replacement)
 	sstring = SString("0123456789");
-	sstring.replace(-1, 4, QStringLiteral("aaaa"));
+	sstring.replace(-1, 4, $("aaaa"));
 	QVERIFY(sstring == SString("0123456789"));
-	sstring.replace(-1, 0, QStringLiteral("bbbb"));
+	sstring.replace(-1, 0, $("bbbb"));
 	QVERIFY(sstring == SString("0123456789"));
-	sstring.replace(15, 0, QStringLiteral("cccc"));
+	sstring.replace(15, 0, $("cccc"));
 	QVERIFY(sstring == SString("0123456789"));
-	sstring.replace(15, 5, QStringLiteral("dddd"));
+	sstring.replace(15, 5, $("dddd"));
 	QVERIFY(sstring == SString("0123456789"));
-	sstring.replace(0, 2, QStringLiteral("eeee"));
+	sstring.replace(0, 2, $("eeee"));
 	QVERIFY(sstring == SString("eeee23456789"));
-	sstring.replace(5, -1, QStringLiteral("dddd"));
+	sstring.replace(5, -1, $("dddd"));
 	QVERIFY(sstring == SString("eeee2dddd"));
 
-	// SString& replace( int index, int len, const SString& replacement )
+	// SString & SString::replace(int index, int len, const SString &replacement)
 	sstring = SString("0123456789");
 	sstring.replace(-1, 4, SString("aaaa"));
 	QVERIFY(sstring == SString("0123456789"));
@@ -158,34 +160,37 @@ SStringTest::testReplace()
 	sstring.replace(5, -1, SString("dddd"));
 	QVERIFY(sstring == SString("eeee2dddd"));
 
-	// SString& replace( const QString& before, const QString& after, Qt::CaseSensitivity cs=Qt::CaseSensitive )
+	// SString & SString::replace(const QString &before, const QString &after, Qt::CaseSensitivity cs=Qt::CaseSensitive)
 	sstring = SString("01234567890123456789");
-	sstring.replace(QStringLiteral("0"), QStringLiteral("A"), Qt::CaseSensitive);
+	sstring.replace($("0"), $("A"), Qt::CaseSensitive);
 	QVERIFY(sstring == SString("A123456789A123456789"));
-	sstring.replace(QStringLiteral("23"), QStringLiteral("B"), Qt::CaseSensitive);
+	sstring.replace($("23"), $("B"), Qt::CaseSensitive);
 	QVERIFY(sstring == SString("A1B456789A1B456789"));
-	sstring.replace(QStringLiteral("89"), QStringLiteral("C"), Qt::CaseSensitive);
+	sstring.replace($("89"), $("C"), Qt::CaseSensitive);
 	QVERIFY(sstring == SString("A1B4567CA1B4567C"));
-	sstring.replace(QStringLiteral("67CA1B"), QStringLiteral("D"), Qt::CaseSensitive);
+	sstring.replace($("67CA1B"), $("D"), Qt::CaseSensitive);
 	QVERIFY(sstring == SString("A1B45D4567C"));
-	sstring.replace(QString(), QStringLiteral("E"), Qt::CaseSensitive);
+	sstring.replace(QString(), $("E"), Qt::CaseSensitive);
 	QVERIFY(sstring == SString("EAE1EBE4E5EDE4E5E6E7ECE"));
 	sstring = SString("3");
-	sstring.replace(QString(), QStringLiteral("AA"), Qt::CaseSensitive);
+	sstring.replace(QString(), $("AA"), Qt::CaseSensitive);
 	QVERIFY(sstring == SString("AA3AA"));
 	sstring = SString();
-	sstring.replace(QString(), QStringLiteral("AA"), Qt::CaseSensitive);
+	sstring.replace(QString(), $("AA"), Qt::CaseSensitive);
 	QVERIFY(sstring == SString("AA"));
+	sstring.setRichString("c<b>a</b>c");
+	sstring.replace($("c"), $("b"));
+	QVERIFY(sstring.richString() == QLatin1String("b<b>a</b>b"));
 
-	// SString& replace( const QString& before, const SString& after, Qt::CaseSensitivity cs=Qt::CaseSensitive )
+	// SString & SString::replace(const QString &before, const SString &after, Qt::CaseSensitivity cs=Qt::CaseSensitive)
 	sstring = SString("01234567890123456789");
-	sstring.replace(QStringLiteral("0"), SString("A", SString::Bold), Qt::CaseSensitive);
+	sstring.replace($("0"), SString("A", SString::Bold), Qt::CaseSensitive);
 	QVERIFY(sstring.richString() == QLatin1String("<b>A</b>123456789<b>A</b>123456789"));
-	sstring.replace(QStringLiteral("23"), SString("B", SString::Italic), Qt::CaseSensitive);
+	sstring.replace($("23"), SString("B", SString::Italic), Qt::CaseSensitive);
 	QVERIFY(sstring.richString() == QLatin1String("<b>A</b>1<i>B</i>456789<b>A</b>1<i>B</i>456789"));
-	sstring.replace(QStringLiteral("89"), SString("C", SString::Underline), Qt::CaseSensitive);
+	sstring.replace($("89"), SString("C", SString::Underline), Qt::CaseSensitive);
 	QVERIFY(sstring.richString() == QLatin1String("<b>A</b>1<i>B</i>4567<u>C</u><b>A</b>1<i>B</i>4567<u>C</u>"));
-	sstring.replace(QStringLiteral("67CA1B"), SString("D", SString::StrikeThrough), Qt::CaseSensitive);
+	sstring.replace($("67CA1B"), SString("D", SString::StrikeThrough), Qt::CaseSensitive);
 	QVERIFY(sstring.richString() == QLatin1String("<b>A</b>1<i>B</i>45<s>D</s>4567<u>C</u>"));
 	sstring.replace(QString(), SString("E"), Qt::CaseSensitive);
 	QVERIFY(sstring.richString() == QLatin1String("E<b>A</b>E1E<i>B</i>E4E5E<s>D</s>E4E5E6E7E<u>C</u>E"));
@@ -196,7 +201,7 @@ SStringTest::testReplace()
 	sstring.replace(QString(), SString("AA", SString::Italic), Qt::CaseSensitive);
 	QVERIFY(sstring.richString() == QLatin1String("<i>AA</i>"));
 
-	// SString& replace( const QRegExp& rx, const QString& after )
+	// SString & SString::replace(const QRegExp &regExp, const QString &replacement)
 	sstring = SString("this is a pretty test");
 	sstring.replace(QRegExp("^this"), "\\2\\0");
 	QVERIFY(sstring.richString() == QLatin1String("\\2this is a pretty test"));
@@ -206,28 +211,69 @@ SStringTest::testReplace()
 	sstring.replace(QRegExp("([\\w]*)"), "[\\1]");
 	QVERIFY(sstring.richString() == QLatin1String("[this][] [is][] [a][] [pretty][] [test][]"));
 
-	// SString& SString::replace( QChar before, QChar after, Qt::CaseSensitivity cs=Qt::CaseSensitive )
+	// SString & SString::replace(const QRegExp &regExp, const QString &replacement)
+	sstring = SString("this is a pretty test");
+	sstring.replace(QRegExp("^this"), "\\2\\0");
+	QVERIFY(sstring.richString() == QLatin1String("\\2this is a pretty test"));
+	sstring.replace(QRegExp("[\\w]+"), "w");
+	QVERIFY(sstring.richString() == QLatin1String("\\w w w w w"));
+	sstring = SString("this is a pretty test");
+	sstring.replace(QRegExp("([\\w]*)"), "[\\1]");
+	QVERIFY(sstring.richString() == QLatin1String("[this][] [is][] [a][] [pretty][] [test][]"));
+	sstring.setRichString("<b>a</b>");
+	sstring.replace(QRegExp("a$"), "b");
+	QVERIFY(sstring.richString() == QLatin1String("<b>b</b>"));
+
+	// SString & SString::replace(const QRegularExpression &regExp, const QString &replacement)
+	sstring = SString("this is a pretty test");
+	sstring.replace(RE$("^this"), "\\2\\0");
+	QVERIFY(sstring.richString() == QLatin1String("\\2this is a pretty test"));
+	sstring.replace(RE$("[\\w]+"), "w");
+	QVERIFY(sstring.richString() == QLatin1String("\\w w w w w"));
+	sstring = SString("this is a pretty test");
+	sstring.replace(RE$("([\\w]*)"), "[\\1]");
+	QEXPECT_FAIL("", "SString will be rewritten - it's too complex/buggy/slow now.", Continue);
+	QVERIFY(sstring.richString() == QLatin1String("[this][] [is][] [a][] [pretty][] [test][]"));
+
+	// SString & SString::replace(const QRegularExpression &regExp, const QString &replacement)
+	sstring = SString("this is a pretty test");
+	sstring.replace(RE$("^this"), "\\2\\0");
+	QVERIFY(sstring.richString() == QLatin1String("\\2this is a pretty test"));
+	sstring.replace(RE$("[\\w]+"), "w");
+	QVERIFY(sstring.richString() == QLatin1String("\\w w w w w"));
+	sstring = SString("this is a pretty test");
+	sstring.replace(RE$("([\\w]*)"), "[\\1]");
+	QEXPECT_FAIL("", "SString will be rewritten - it's too complex/buggy/slow now.", Continue);
+	QVERIFY(sstring.richString() == QLatin1String("[this][] [is][] [a][] [pretty][] [test][]"));
+	sstring.setRichString("<b>a</b>");
+	sstring.replace(RE$("a$"), "b");
+	QVERIFY(sstring.richString() == QLatin1String("<b>b</b>"));
+
+	// SString & SString::replace(QChar before, QChar after, Qt::CaseSensitivity cs=Qt::CaseSensitive)
 	sstring.setRichString("<b>This </b>Is <i>a preTtI</i> testi");
 	sstring.replace('I', 'X', Qt::CaseSensitive);
 	QVERIFY(sstring.richString() == QLatin1String("<b>This </b>Xs <i>a preTtX</i> testi"));
 	sstring.replace('T', 'w', Qt::CaseInsensitive);
 	QVERIFY(sstring.richString() == QLatin1String("<b>whis </b>Xs <i>a prewwX</i> weswi"));
+	sstring.setRichString("c<b>a</b>c");
+	sstring.replace('c', 'b');
+	QVERIFY(sstring.richString() == QLatin1String("b<b>a</b>b"));
 
-	// SString& SString::replace( QChar ch, const QString& after, Qt::CaseSensitivity cs=Qt::CaseSensitive )
+	// SString & SString::replace(QChar ch, const QString &after, Qt::CaseSensitivity cs=Qt::CaseSensitive)
 	sstring.setRichString("<b>This </b>Is <i>a preTtI</i> testi");
-	sstring.replace('i', QStringLiteral("AA"), Qt::CaseInsensitive);
+	sstring.replace('i', $("AA"), Qt::CaseInsensitive);
 	QVERIFY(sstring.richString() == QLatin1String("<b>ThAAs </b>AAs <i>a preTtAA</i> testAA"));
-	sstring.replace('T', QStringLiteral("XX"), Qt::CaseSensitive);
+	sstring.replace('T', $("XX"), Qt::CaseSensitive);
 	QVERIFY(sstring.richString() == QLatin1String("<b>XXhAAs </b>AAs <i>a preXXtAA</i> testAA"));
 
-	// SString& SString::replace( QChar ch, const SString& after, Qt::CaseSensitivity cs=Qt::CaseSensitive )
+	// SString & SString::replace(QChar ch, const SString &after, Qt::CaseSensitivity cs=Qt::CaseSensitive)
 	sstring.setRichString("<b>This </b>Is <i>a preTtI</i> testi");
 	sstring.replace('i', SString("A", SString::Bold));
 	QVERIFY(sstring.richString() == QLatin1String("<b>ThAs </b>Is <i>a preTtI</i> test<b>A</b>"));
 	sstring.replace('T', SString("X", SString::Underline), Qt::CaseInsensitive);
 	QVERIFY(sstring == SString().setRichString("<u>X</u><b>hAs </b>Is <i>a pre</i><u>XX</u><i>I</i> <u>X</u>es<u>X</u><b>A</b>"));
 
-	// SString& remove( int index, int len );
+	// SString & SString::remove(int index, int len);
 	sstring.setRichString("<b>012</b><i>345</i><u>678</u><s>9</s>");
 	sstring.remove(0, 0);
 	QVERIFY(sstring.richString() == QLatin1String("<b>012</b><i>345</i><u>678</u><s>9</s>"));
@@ -244,7 +290,7 @@ SStringTest::testReplace()
 	sstring.remove(0, -1);
 	QVERIFY(sstring.richString().isEmpty());
 
-	// SString& remove( const QString& str, Qt::CaseSensitivity cs=Qt::CaseSensitive );
+	// SString & SString::remove(const QString &str, Qt::CaseSensitivity cs=Qt::CaseSensitive);
 	sstring.setRichString("<b>012</b><i>012</i><u>345</u><s>345</s>");
 	sstring.remove("013");
 	QVERIFY(sstring.richString() == QLatin1String("<b>012</b><i>012</i><u>345</u><s>345</s>"));
@@ -253,7 +299,7 @@ SStringTest::testReplace()
 	sstring.remove("2345");
 	QVERIFY(sstring.richString() == QLatin1String("<b>2</b><s>345</s>"));
 
-	// SString& remove( const QRegExp& rx );
+	// SString & SString::remove(const QRegExp &regExp);
 	sstring.setRichString("<b>012</b><i>012</i><u>345</u><s>345</s>");
 	sstring.remove(QRegExp("^013"));
 	QVERIFY(sstring.richString() == QLatin1String("<b>012</b><i>012</i><u>345</u><s>345</s>"));
@@ -264,24 +310,12 @@ SStringTest::testReplace()
 	sstring.remove(QRegExp("\\d+"));
 	QVERIFY(sstring.richString().isEmpty());
 
-	// SString& remove( QChar ch, Qt::CaseSensitivity cs=Qt::CaseSensitive );
+	// SString & SString::remove(QChar ch, Qt::CaseSensitivity cs=Qt::CaseSensitive);
 	sstring.setRichString("<b>This </b>Is <i>a preTtI</i> testi");
 	sstring.remove(QChar('T'), Qt::CaseSensitive);
 	QVERIFY(sstring.richString() == QLatin1String("<b>his </b>Is <i>a pretI</i> testi"));
 	sstring.remove(QChar('i'), Qt::CaseInsensitive);
 	QVERIFY(sstring.richString() == QLatin1String("<b>hs </b>s <i>a pret</i> test"));
-
-	sstring.setRichString("<b>a</b>");
-	sstring.replace(QRegExp("a$"), "b");
-	QVERIFY(sstring.richString() == QLatin1String("<b>b</b>"));
-
-	sstring.setRichString("c<b>a</b>c");
-	sstring.replace("c", "b");      // krazy:exclude=c++/doublequote_chars
-	QVERIFY(sstring.richString() == QLatin1String("b<b>a</b>b"));
-
-	sstring.setRichString("c<b>a</b>c");
-	sstring.replace('c', 'b');
-	QVERIFY(sstring.richString() == QLatin1String("b<b>a</b>b"));
 }
 
 QTEST_GUILESS_MAIN(SStringTest);
