9d63639e1717595bb4c6dcb667a462e01cbb5262
Friedrich W. H. Kossebau <kossebau@kde.org>
2023-06-26 15:22:15 +0200

    Port away from deprecated KUrlRequester::setFilter
    GIT_SILENT

--- src/speechplugins/pocketsphinx/pocketsphinxconfigwidget.cpp
+++ src/speechplugins/pocketsphinx/pocketsphinxconfigwidget.cpp
@@ -7,6 +7,7 @@
 #include "pocketsphinxconfigwidget.h"
 
 #include <KLocalizedString>
+#include <kio_version.h>
 
 using namespace SubtitleComposer;
 
@@ -14,8 +15,19 @@ PocketSphinxConfigWidget::PocketSphinxConfigWidget(QWidget *parent)
 	: QWidget(parent)
 {
 	setupUi(this);
+#if KIO_VERSION >= QT_VERSION_CHECK(5, 108, 0)
+	kcfg_lexiconFile->setNameFilters({
+		i18n("Sphinx Dictionaries") + QLatin1String(" (*.dict *.dic)"),
+		i18n("All Files") + QLatin1String(" (*)"),
+	});
+	kcfg_trigramModelFile->setNameFilters({
+		i18n("Trigram Models") + QStringLiteral(" (*.lm.bin *.lm)"),
+		i18n("All Files") + QLatin1String(" (*)"),
+	});
+#else
 	kcfg_lexiconFile->setFilter(QLatin1String("*.dict *.dic|") + i18n("Sphinx Dictionaries") + QLatin1String("\n*|") + i18n("All Files"));
 	kcfg_trigramModelFile->setFilter(QStringLiteral("*.lm.bin *.lm|") + i18n("Trigram Models") + QLatin1String("\n*|") + i18n("All Files"));
+#endif
 }
 
 PocketSphinxConfigWidget::~PocketSphinxConfigWidget()
