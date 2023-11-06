maxrd2/SubtitleComposer

commit a15fb770aaad6ee680a8b1398f1d137f218b1112

Waveform: Fixed misalignment due to rounding error
WaveBuffer was using samples per millisecond, on 44100KHz audio the
value was rounded from 2.756 to 2. As a consequence whole waveform was
stretched by 2.756/2

src/gui/waveform/wavebuffer.cpp
@@ -54,7 +54,7 @@ WaveBuffer::WaveBuffer(WaveformWidget *parent)
 	  m_waveformChannels(0),
 	  m_waveformChannelSize(0),
 	  m_waveform(nullptr),
-	  m_samplesMsec(0),
+	  m_samplesSec(0),
 	  m_wfFrame(nullptr),
 	  m_zoomBuffer(new ZoomBuffer(this))
 {
@@ -67,7 +67,7 @@ WaveBuffer::WaveBuffer(WaveformWidget *parent)
 quint32
 WaveBuffer::millisPerPixel() const
 {
-	return m_zoomBuffer->samplesPerPixel() / m_samplesMsec;
+	return m_zoomBuffer->samplesPerPixel() / m_samplesSec / 1000;
 }
 
 quint32
@@ -109,7 +109,7 @@ WaveBuffer::clearAudioStream()
 		m_waveform = nullptr;
 		m_waveformChannelSize = 0;
 		m_waveformChannels = 0;
-		m_samplesMsec = 0;
+		m_samplesSec = 0;
 	}
 }
 
@@ -160,7 +160,6 @@ WaveBuffer::onStreamData(const void *buffer, qint32 size, const WaveFormat *wave
 			m_samplesSec >>= 1;
 			sampleShift++;
 		}
-		m_samplesMsec = m_samplesSec / 1000;
 		m_waveformChannels = waveFormat->channels();
 		m_waveformChannelSize = m_samplesSec * (m_waveformDuration + 60); // added 60sec as duration might be wrong
 		m_waveform = new SAMPLE_TYPE *[m_waveformChannels];

src/gui/waveform/wavebuffer.h
@@ -44,14 +44,14 @@ public:
 	 */
 	inline quint32 waveformDuration() const { return m_waveformDuration; }
 
-	inline quint32 lengthMillis() const { return m_waveformChannelSize / m_samplesMsec; }
+	inline quint32 lengthMillis() const { return m_waveformChannelSize / m_samplesSec / 1000; }
 	inline quint32 lengthSamples() const { return m_waveformChannelSize; }
 
 	/**
 	 * @brief sampleRateMillis
-	 * @return number of samples per millisecond
+	 * @return number of samples per second
 	 */
-	inline quint32 sampleRateMillis() const { return m_samplesMsec; }
+	inline quint32 sampleRate() const { return m_samplesSec; }
 
 	quint32 millisPerPixel() const;
 
@@ -91,7 +91,6 @@ private:
 	quint32 m_waveformChannelSize;
 	SAMPLE_TYPE **m_waveform;
 
-	quint32 m_samplesMsec;
 	quint32 m_samplesSec;
 
 	struct WaveformFrame *m_wfFrame;

src/gui/waveform/waveformwidget.cpp
@@ -192,12 +192,12 @@ WaveformWidget::setZoom(quint32 val)
 
 	m_wfBuffer->zoomBuffer()->setZoomScale(val);
 
-	const quint32 samMS = m_wfBuffer->sampleRateMillis();
-	if(samMS) {
-		const qint32 msSpanOld = m_zoom * span / samMS;
+	const quint32 samRt = m_wfBuffer->sampleRate();
+	if(samRt) {
+		const qint32 msSpanOld = m_zoom * span * 1000 / samRt;
 		m_zoom = val;
 
-		const quint32 msSpanNew = val * span / samMS;
+		const quint32 msSpanNew = val * span * 1000 / samRt;
 		m_timeStart.shift((msSpanOld - qint32(msSpanNew)) / 2);
 		m_timeEnd = m_timeStart.shifted(msSpanNew);
 		handleTimeUpdate(msSpanNew);
@@ -239,9 +239,9 @@ WaveformWidget::handleTimeUpdate(quint32 msSpan)
 void
 WaveformWidget::onWaveformResize(quint32 span)
 {
-	const quint32 samMS = m_wfBuffer->sampleRateMillis();
-	if(samMS) {
-		const double windowSize = m_zoom * span / samMS;
+	const quint32 samRt = m_wfBuffer->sampleRate();
+	if(samRt) {
+		const double windowSize = m_zoom * span * 1000 / samRt;
 		m_timeEnd = m_timeStart.shifted(windowSize);
 		handleTimeUpdate(windowSize);
 		updateActions();
@@ -458,7 +458,7 @@ WaveformWidget::draggableAt(double posTime, WaveSubtitle **result)
 	if(result)
 		*result = nullptr;
 
-	if(!m_wfBuffer->sampleRateMillis())
+	if(!m_wfBuffer->sampleRate())
 		return DRAG_NONE;
 
 	double msTolerance = double(10 * m_wfBuffer->millisPerPixel());

src/gui/waveform/zoombuffer.cpp
@@ -225,8 +225,8 @@ ZoomBuffer::zoomedBuffer(quint32 timeStart, quint32 timeEnd, WaveZoomData **buff
 
 	QMutexLocker l(&m_reqMutex);
 
-	m_reqStart = qMin(timeStart * m_waveBuffer->sampleRateMillis() / m_samplesPerPixel, m_waveformZoomedSize);
-	m_reqEnd = qMin(timeEnd * m_waveBuffer->sampleRateMillis() / m_samplesPerPixel, m_waveformZoomedSize);
+	m_reqStart = qMin(timeStart * m_waveBuffer->sampleRate() / m_samplesPerPixel / 1000, m_waveformZoomedSize);
+	m_reqEnd = qMin(timeEnd * m_waveBuffer->sampleRate() / m_samplesPerPixel / 1000, m_waveformZoomedSize);
 	m_reqLen = bufLen;
 
 	for(quint16 ch = 0; ch < m_waveBuffer->channels(); ch++)

commit 34b37cd4851cbb8f7d660f73ef0525f5b9225a4e

Waveform: Fixed inability to drag show/hide time
Regression introduced with a15fb770aaad6ee680a8b1398f1d137f218b1112

src/gui/waveform/wavebuffer.cpp
@@ -67,7 +67,7 @@ WaveBuffer::WaveBuffer(WaveformWidget *parent)
 quint32
 WaveBuffer::millisPerPixel() const
 {
-	return m_zoomBuffer->samplesPerPixel() / m_samplesSec / 1000;
+	return m_zoomBuffer->samplesPerPixel() * 1000 / m_samplesSec;
 }
 
 quint32

src/gui/waveform/wavebuffer.h
@@ -44,7 +44,7 @@ public:
 	 */
 	inline quint32 waveformDuration() const { return m_waveformDuration; }
 
-	inline quint32 lengthMillis() const { return m_waveformChannelSize / m_samplesSec / 1000; }
+	inline quint32 lengthMillis() const { return m_waveformChannelSize * 1000 / m_samplesSec; }
 	inline quint32 lengthSamples() const { return m_waveformChannelSize; }
 
 	/**

src/gui/waveform/waveformwidget.cpp
@@ -461,7 +461,7 @@ WaveformWidget::draggableAt(double posTime, WaveSubtitle **result)
 	if(!m_wfBuffer->sampleRate())
 		return DRAG_NONE;
 
-	double msTolerance = double(10 * m_wfBuffer->millisPerPixel());
+	double msTolerance = 10. * m_wfBuffer->zoomBuffer()->samplesPerPixel() * 1000. / m_wfBuffer->sampleRate();
 
 	DragPosition dragMode = DRAG_NONE;
 	for(WaveSubtitle *sub: m_visibleLines) {